8624fa92b7b06ab601c38fa47f108d0c2d4bf9ce
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-31 16:07:35 -0400

    Scaffold Knock integration

--- apps/app/app/(authenticated)/layout.tsx
+++ apps/app/app/(authenticated)/layout.tsx
@@ -2,6 +2,7 @@ import arcjet, { detectBot } from '@/lib/arcjet';
 import { request } from '@arcjet/next';
 import { auth, currentUser } from '@clerk/nextjs/server';
 import { SidebarProvider } from '@repo/design-system/components/ui/sidebar';
+import { NotificationsProvider } from '@repo/design-system/providers/notifications';
 import { showBetaFeature } from '@repo/feature-flags';
 import type { ReactElement, ReactNode } from 'react';
 import { PostHogIdentifier } from './components/posthog-identifier';
@@ -41,21 +42,23 @@ const AppLayout = async ({
   const betaFeature = await showBetaFeature();
 
   if (!user) {
-    redirectToSignIn();
+    return redirectToSignIn();
   }
 
   return (
-    <SidebarProvider>
-      <GlobalSidebar>
-        {betaFeature && (
-          <div className="m-4 rounded-full bg-success p-1.5 text-center text-sm text-success-foreground">
-            Beta feature now available
-          </div>
-        )}
-        {children}
-      </GlobalSidebar>
-      <PostHogIdentifier />
-    </SidebarProvider>
+    <NotificationsProvider userId={user.id}>
+      <SidebarProvider>
+        <GlobalSidebar>
+          {betaFeature && (
+            <div className="m-4 rounded-full bg-success p-1.5 text-center text-sm text-success-foreground">
+              Beta feature now available
+            </div>
+          )}
+          {children}
+        </GlobalSidebar>
+        <PostHogIdentifier />
+      </SidebarProvider>
+    </NotificationsProvider>
   );
 };
 
