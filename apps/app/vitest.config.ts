ca2d1a0fd55335e4e8b9763fa06dba6bb61f7501
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-04 19:35:28 -0500

    Scaffold standalone testing package

--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,9 +1,1 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
-});
+export { default } from '@repo/testing';
